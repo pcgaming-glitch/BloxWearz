5d7e0c37a1c285d92846e1ec5ad5859880eccb20
pcgaming-glitch <dauntaythomas@yahoo.com>
2026-02-11 09:28:35 +0100

    Create Footer.jsx

---
+++ components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>BloxWearz © {new Date().getFullYear()}</p>
+      <p>Made for Roblox creators</p>
+    </footer>
+  );
+}
