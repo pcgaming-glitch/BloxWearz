7e317dafb49e4ab02b45d37952a7f62b57bff96d
pcgaming-glitch <dauntaythomas@yahoo.com>
2026-02-11 08:59:15 +0100

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+export default function HomePage() {
+  return (
+    <div className="home">
+      <h1>Welcome to BloxWearz</h1>
+      <p>Your Roblox clothing marketplace.</p>
+
+      <a href="/shop" className="btn">
+        Visit Shop
+      </a>
+    </div>
+  );
+}
